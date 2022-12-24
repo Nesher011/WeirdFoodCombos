0fc5c8e2a0d35352228fb967c4c89276cf6e438d
Jędrzej Kłyza <neshernos@gmail.com>
2022-12-24 22:00:15 +0100

    Created RecipeEntity

---
+++ WeirdFoodCombosBackend/Entities/Recipe.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeirdFoodCombosBackend.Entities
+{
+    public class Recipe:BaseEntity
+    {
+        public string Name { get; set; }
+        [Range(0,5)]
+        public int Difficulty { get; set; }
+    }
+}
