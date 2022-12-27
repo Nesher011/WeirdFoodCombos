64625e9d1fd5b4936852fa4a492e83b407c812ee
Jędrzej Kłyza <neshernos@gmail.com>
2022-12-27 16:49:11 +0100

    Created RecipeController

---
+++ WeirdFoodCombosBackend/Controllers/RecipeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeirdFoodCombosBackend.Dtos;
+using WeirdFoodCombosBackend.Entities;
+using WeirdFoodCombosBackend.Interfaces;
+using WeirdFoodCombosBackend.Repositories;
+
+namespace WeirdFoodCombosBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RecipeController : ControllerBase
+    {
+        private readonly IRecipeRepository _recipeRepository;
+
+        public RecipeController(IRecipeRepository recipeRepository)
+        {
+            _recipeRepository = recipeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Recipe>>> GetAll()
+        {
+            return Ok(await _recipeRepository.GetAll());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Recipe>> Create([FromBody] RecipeDto recipeDto)
+        {
+            var recipe = await _recipeRepository.Create(recipeDto);
+            if (recipe == null)
+                throw new NotImplementedException();
+
+            return Ok(recipe);
+        }
+    }
+}
