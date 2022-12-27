d008b8568e71f196f367ed162bb2080333993292
Jędrzej Kłyza <neshernos@gmail.com>
2022-12-27 16:48:50 +0100

    Added Database and did initial migration

---
+++ WeirdFoodCombosBackend/Databases/WeirdFoodCombosContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeirdFoodCombosBackend.Entities;
+
+namespace WeirdFoodCombosBackend.Databases
+{
+    public class WeirdFoodCombosContext:DbContext
+    {
+        public DbSet<Recipe> Recipies => Set<Recipe>();
+
+        public WeirdFoodCombosContext(DbContextOptions<WeirdFoodCombosContext> options):base(options)
+        {
+               
+        }
+    }
+}
