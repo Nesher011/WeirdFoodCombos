db803db8c97e3f1d6ce23815519c7d13c57d8a7c
Jędrzej Kłyza <neshernos@gmail.com>
2022-12-27 16:52:23 +0100

    cleanup

--- WeirdFoodCombosFrontend/src/app/app.module.ts
+++ WeirdFoodCombosFrontend/src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RecipeComponent } from './components/recipes/recipe.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatTableModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
