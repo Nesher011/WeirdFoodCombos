adb0943d555c7099e7cd17d396d7d98d3830db5b
Jędrzej Kłyza <neshernos@gmail.com>
2022-12-28 23:17:05 +0100

    Implemented get, put, delete HTTP methods

--- WeirdFoodCombosBackend/Controllers/RecipeController.cs
+++ WeirdFoodCombosBackend/Controllers/RecipeController.cs
@@ -23,14 +23,38 @@ namespace WeirdFoodCombosBackend.Controllers
             return Ok(await _recipeRepository.GetAll());
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Recipe>> GetById(Guid id)
+        {
+            return await _recipeRepository.GetById(id) is RecipeDto r ? Ok(r) : NotFound();
+        }
+
         [HttpPost]
         public async Task<ActionResult<Recipe>> Create([FromBody] RecipeDto recipeDto)
         {
             var recipe = await _recipeRepository.Create(recipeDto);
             if (recipe == null)
-                throw new NotImplementedException();
+                return BadRequest();
 
             return Ok(recipe);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Recipe>> Update([FromBody] RecipeDto recipeDto)
+        {
+            var recipe = await _recipeRepository.Update(recipeDto);
+
+            if (recipe == null)
+                return BadRequest();
+
+            return Ok(recipe);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete([FromRoute] Guid id)
+        {
+            await _recipeRepository.Delete(id);
+            return NoContent();
         }
     }
 }
