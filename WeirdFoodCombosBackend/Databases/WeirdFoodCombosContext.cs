daefe3df3501864c4d8ac3f44a01c5a82f1f1280
Jędrzej Kłyza <neshernos@gmail.com>
2022-12-31 16:40:56 +0100

    changed styling

--- WeirdFoodCombosBackend/Databases/WeirdFoodCombosContext.cs
+++ WeirdFoodCombosBackend/Databases/WeirdFoodCombosContext.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using WeirdFoodCombosBackend.Entities;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace WeirdFoodCombosBackend.Databases
 {
-    public class WeirdFoodCombosContext:DbContext
+    public class WeirdFoodCombosContext : DbContext
     {
         public DbSet<Recipe> Recipies => Set<Recipe>();
 
-        public WeirdFoodCombosContext(DbContextOptions<WeirdFoodCombosContext> options):base(options)
+        public WeirdFoodCombosContext(DbContextOptions<WeirdFoodCombosContext> options) : base(options)
         {
-               
         }
     }
 }
