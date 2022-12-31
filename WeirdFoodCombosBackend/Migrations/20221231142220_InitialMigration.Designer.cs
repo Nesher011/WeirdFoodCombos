8d1f9edcda7523dee503175800e241eef9729166
Jędrzej Kłyza <neshernos@gmail.com>
2022-12-31 16:41:54 +0100

    Initial Migration

--- WeirdFoodCombosBackend/Migrations/20221231142220_InitialMigration.Designer.cs
+++ WeirdFoodCombosBackend/Migrations/20221231142220_InitialMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WeirdFoodCombosBackend.Databases;
 
@@ -11,9 +12,11 @@ using WeirdFoodCombosBackend.Databases;
 namespace WeirdFoodCombosBackend.Migrations
 {
     [DbContext(typeof(WeirdFoodCombosContext))]
-    partial class WeirdFoodCombosContextModelSnapshot : ModelSnapshot
+    [Migration("20221231142220_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
