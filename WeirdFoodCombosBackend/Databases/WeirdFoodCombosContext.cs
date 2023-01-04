ddffc9f48b716c7c5bd1ec8bb5fde2ac4723062f
Jędrzej Kłyza <neshernos@gmail.com>
2023-01-04 09:42:31 +0100

    configured one to many relationship

--- WeirdFoodCombosBackend/Databases/WeirdFoodCombosContext.cs
+++ WeirdFoodCombosBackend/Databases/WeirdFoodCombosContext.cs
@@ -11,5 +11,17 @@ namespace WeirdFoodCombosBackend.Databases
         public WeirdFoodCombosContext(DbContextOptions<WeirdFoodCombosContext> options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Ingredient>()
+                .HasOne(e => e.Recipe)
+                .WithMany(e => e.Ingredients)
+                .HasForeignKey(e => e.RecipeId);
+            modelBuilder.Entity<Step>()
+                .HasOne(e => e.Recipe)
+                .WithMany(e => e.Steps)
+                .HasForeignKey(e => e.RecipeId);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
